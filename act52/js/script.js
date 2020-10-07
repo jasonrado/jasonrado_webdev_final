e67aea9b6189c583a0a868c2118824c071bbdaee
jasonrado <70185340+jasonrado@users.noreply.github.com>
2020-10-07 21:38:18 +0800

    Update script.js

--- act52/js/script.js
+++ act52/js/script.js
@@ -4,7 +4,7 @@ function Alert(){
   var mail = document.getElementById("email2").value;
   var firstname = document.getElementById("fname").value;
   var lastname = document.getElementById("lname").value;
-  var thankyou = firstname 
+
 
 if(document.getElementById('termsandcon2').checked == true)
 {
