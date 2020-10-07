9c9dd95a79baa5bb8fc974e7c8821a462a6e53e1
jasonrado <70185340+jasonrado@users.noreply.github.com>
2020-10-07 22:03:40 +0800

    Update script.js

--- act52/js/script.js
+++ act52/js/script.js
@@ -4,14 +4,16 @@ function Alert(){
   var mail = document.getElementById("email2").value;
   var firstname = document.getElementById("fname").value;
   var lastname = document.getElementById("lname").value;
+  var dateee = document.getElementById("datee").value;
 
 
 if(document.getElementById('termsandcon2').checked == true)
 {
   if (/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(form.email2.value))
   {
-    if(user=="" | pass=="" | mail=="" | firstname=="" |lastname==""){
+    if(user=="" | pass=="" | mail=="" | firstname=="" |lastname=="" | dateee== document.getElementById("datee").defaultValue){
  alert('Please Submit Everything');
+       event.preventDefault();
   }
   else{
     alert('Thank you ' + firstname + ' ' +lastname);
@@ -24,5 +26,6 @@ if(document.getElementById('termsandcon2').checked == true)
 }
 else{
   alert('Please Accept the terms and conditions');
+   event.preventDefault();
 }
 }
